7a4cbdde91f7d6e094cd176ec240035860ac44c4
Michael Tong <michael_tong@outlook.com>
2018-07-08 15:23:30 -0700

    Added sound effect and moved rocket

--- LaunchTheShip/ViewController.swift
+++ LaunchTheShip/ViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import AVFoundation // Allows to play sound
 
 class ViewController: UIViewController {
+    
+    // Initialize these connections
 
     @IBOutlet weak var darkBlueBG: UIImageView!
     @IBOutlet weak var powerBtn: UIButton!
@@ -16,18 +19,43 @@ class ViewController: UIViewController {
     @IBOutlet weak var rocket: UIImageView!
     @IBOutlet weak var launchLbl: UILabel!
     
+    var player: AVAudioPlayer!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // First load
+        
+        // Get the path for the sound
+        
+        let path = Bundle.main.path(forResource: "LaunchTheShip", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do { // The error if cannot get audio
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
     }
 
 
-
+    // If power button is pressed
+    
     @IBAction func powerBtnPressed(_ sender: Any) {
         cloudHolder.isHidden = false
         darkBlueBG.isHidden = true
         powerBtn.isHidden = true
+        
+        // Play sound effect
+        player.play()
+        
+        // Move rocket
+        UIView.animate(withDuration: 2.3, animations: {
+            self.rocket.frame = CGRect(x: 0, y: 20, width: 375, height: 186)
+        }) { (finished) in
+            self.launchLbl.isHidden = false
+        }
+        
     }
     
     
