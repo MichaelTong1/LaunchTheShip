1593d71043451ee1a70e94668d8630de6534979b
Michael Tong <michael_tong@outlook.com>
2018-07-08 19:05:19 -0700

    After finishing the tutorial, I added a second button and sound.

--- LaunchTheShip/ViewController.swift
+++ LaunchTheShip/ViewController.swift
@@ -18,10 +18,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var cloudHolder: UIView!
     @IBOutlet weak var rocket: UIImageView!
     @IBOutlet weak var launchLbl: UILabel!
+    @IBOutlet weak var launchBox: UIView!
+    @IBOutlet weak var launchBtn: UIButton!
+    @IBOutlet weak var clouds: UIImageView!
     
     
     var player: AVAudioPlayer!
-    
+    var player2: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,9 +34,13 @@ class ViewController: UIViewController {
         
         let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
         let url = URL(fileURLWithPath: path)
+        let path2 = Bundle.main.path(forResource: "pew", ofType: "wav")!
+        let url2 = URL(fileURLWithPath: path2)
         do { // The error if cannot get audio
             player = try AVAudioPlayer(contentsOf: url)
             player.prepareToPlay()
+            player2 = try AVAudioPlayer(contentsOf: url2)
+            player2.prepareToPlay()
         } catch let error as NSError {
             print(error.description)
         }
@@ -52,14 +59,36 @@ class ViewController: UIViewController {
         
         // Move rocket
         UIView.animate(withDuration: 2.3, animations: {
-            self.rocket.frame = CGRect(x: 0, y: 140, width: 375, height: 402)
+            self.rocket.frame = CGRect(x: 0, y: 480, width: 375, height: 0)
         }) { (finished) in
             self.launchLbl.isHidden = false
+            self.launchBox.isHidden = false
+            self.launchBtn.isHidden = false
         }
         
     }
     
+    @IBAction func launchBtnPressesd(_ sender: Any) {
+        
+        player2.play()
+        
+        self.launchBtn.isHidden = true
+        self.launchBox.isHidden = true
+        self.launchLbl.isHidden = true
+        self.clouds.isHidden = true
+        
+        UIView.animate(withDuration: 1.0, animations: {
+            self.rocket.frame = CGRect(x: 0, y: 900, width: 375, height: 0)
+        })
+            UIView.animate(withDuration: 5.0, animations: {
+                self.rocket.frame = CGRect(x: (-100), y: 850, width: 375, height: 0)
+            })
+            UIView.animate(withDuration: 6.0, animations: {
+                self.rocket.frame = CGRect(x: 100, y: 800, width: 375, height: 0)
+            })
+        }
     
+    }
 
-}
+
 
